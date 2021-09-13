9ee79c5c4a6f5b7e2e6354860dbc1faf8cd917aa
Heloisa Guimaraes <heloisaguimaraes86@gmail.com>
2021-09-12 23:22:41 -0300

    fix information

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -16,7 +16,7 @@ const Dashboard = () => {
                         <BarChart />
                     </div>
                     <div className="col-sm-6">
-                        <h5 className="text-center text-secondary">Taxa de sucesso (%)</h5>
+                        <h5 className="text-center text-secondary">Todas vendas</h5>
                         <DonutChart />
                     </div>
                 </div>
